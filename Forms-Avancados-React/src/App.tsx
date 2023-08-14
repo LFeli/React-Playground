9edd8938110e913e33122a0471a13df2b3802eee
Lucas Felipe <lucasfelipemk@gmail.com>
2023-08-14 17:43:03 -0300

    feat: doing content transformation using zod

--- Forms-Avancados-React/src/App.tsx
+++ Forms-Avancados-React/src/App.tsx
@@ -4,9 +4,20 @@ import { z } from "zod";
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const CreateUserFormSchema = z.object({
+  name: z.string()
+  .nonempty('O nome e obrigatório')
+  .transform(name => {
+    return name.trim().split(' ').map(word => {
+      return word[0].toLocaleUpperCase().concat(word.substring(1))
+    }).join(' ')
+  }),
   email: z.string()
     .nonempty('O e-mail e obrigatório')
-    .email('Formato de e-mail invalido'),
+    .email('Formato de e-mail invalido')
+    .toLowerCase()
+    .refine(email =>{
+      return email.endsWith('@teste.com')
+    }, 'O email precisa ser do @teste'), 
   password: z.string()
     .min(6, 'A senha precisa de no mínimo 6 caracteres'),
 })
@@ -34,8 +45,19 @@ export function App() {
       onSubmit={handleSubmit(createUser)}
       className="flex flex-col gap-4 w-full max-w-xs"
     >
+
       <div className="flex flex-col gap-1">
-        <label htmlFor="">E-mail</label>
+        <label htmlFor="name">Nome</label>
+        <input 
+        type="text" 
+        className="border boder-zinc-300 shadow-sm rounded h-10 px-3 dark:bg-zinc-900 text-white border-none"
+        {...register('name')}
+        />
+        {errors.name && <span>{errors.name.message}</span> }
+      </div>
+
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email">E-mail</label>
         <input 
         type="email" 
         className="border boder-zinc-300 shadow-sm rounded h-10 px-3 dark:bg-zinc-900 text-white border-none"
@@ -45,7 +67,7 @@ export function App() {
       </div>
 
       <div className="flex flex-col gap-1">
-        <label htmlFor="">Senha</label>
+        <label htmlFor="password">Senha</label>
         <input 
         type="password" 
         className="border boder-zinc-300 shadow-sm rounded h-10 px-3 dark:bg-zinc-900 text-white border-none"
